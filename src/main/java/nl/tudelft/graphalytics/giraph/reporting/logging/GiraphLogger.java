0dd2962bf6a62b45fabb6c961e78cd95efdea809
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-12-18 23:12:10 +0100

    Fixed Granula Giraph integration

--- src/main/java/nl/tudelft/graphalytics/giraph/reporting/logging/GiraphLogger.java
+++ src/main/java/nl/tudelft/graphalytics/giraph/reporting/logging/GiraphLogger.java
@@ -15,40 +15,35 @@
  */
 package nl.tudelft.graphalytics.giraph.reporting.logging;
 
-import org.apache.hadoop.yarn.client.cli.LogsCLI;
-import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
-import java.io.*;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by wlngai on 9-9-15.
  */
 public class GiraphLogger extends GraphalyticLogger {
 
-    protected static Level platformLogLevel = Level.INFO;
+	protected static Level platformLogLevel = Level.INFO;
 
-    public static void startPlatformLogging(Path fileName) {
-        Logger.getRootLogger().removeAllAppenders();
-        FileAppender fa = new FileAppender();
-        fa.setName("FileLogger");
-        fa.setFile(fileName.toString());
-        fa.setLayout(new PatternLayout("%d [%t] %-5p[%c{1} (%M(%L))] %m%n"));
-        fa.setThreshold(platformLogLevel);
-        fa.setAppend(true);
-        fa.activateOptions();
-        Logger.getRootLogger().addAppender(fa);
-        waitInterval(1);
-    }
+	public static void startPlatformLogging(Path fileName) {
+		Logger.getRootLogger().removeAllAppenders();
+		FileAppender fa = new FileAppender();
+		fa.setName("FileLogger");
+		fa.setFile(fileName.toString());
+		fa.setLayout(new PatternLayout("%d [%t] %-5p[%c{1} (%M(%L))] %m%n"));
+		fa.setThreshold(platformLogLevel);
+		fa.setAppend(true);
+		fa.activateOptions();
+		Logger.getRootLogger().addAppender(fa);
+		waitInterval(1);
+	}
 
-    public static void stopPlatformLogging() {
-        Logger.getRootLogger().removeAllAppenders();
-    }
+	public static void stopPlatformLogging() {
+		Logger.getRootLogger().removeAllAppenders();
+	}
 
 }
